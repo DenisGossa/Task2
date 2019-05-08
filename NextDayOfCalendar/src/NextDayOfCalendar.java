82021835d294b45f7d13bc77b67e3d9567889d94
Denis Gossa <50202499+DenisGossa@users.noreply.github.com>
2019-05-08 21:50:39 +0300

    Update NextDayOfCalendar.java

--- NextDayOfCalendar/src/NextDayOfCalendar.java
+++ NextDayOfCalendar/src/NextDayOfCalendar.java
@@ -129,7 +129,6 @@ public class NextDayOfCalendar {
 	
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		NextDayOfCalendar tcalendar=new NextDayOfCalendar();
 		
 		tcalendar.setCurrentDate();
